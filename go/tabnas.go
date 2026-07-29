34193169faaa1dc23953fbd5682a451ee9a18fdc
Richard Rodger <richard@ricebridge.com>
2026-07-29 10:03:00 +0100

    go: v0.4.0 (OrderedMap insertion-order; require tabnas deps @v0.4.0)

--- go/tabnas.go
+++ go/tabnas.go
@@ -14,7 +14,7 @@ import (
 )
 
 // Version is the current version of the tabnas Go module.
-const Version = "0.3.2"
+const Version = "0.4.0"
 
 // Error message templates matching TypeScript defaults (ts/src/defaults.ts).
 // Values are injected with StrInject using {key} placeholders.
